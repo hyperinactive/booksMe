39dcfa8e769ec46638a6f0169567e1dc51f10318
hyperinactive <hyperinactive.box@gmail.com>
2020-08-19 00:00:21 +0200

    Updated scripts: filtering by isbn, author and title works

--- public/scripts/filter.js
+++ public/scripts/filter.js
@@ -1,50 +1,119 @@
 $("document").ready(() => {
 
-    $("#filter-btn-author").click(() => {
+    setFilterListeners();
+
+    function setFilterListeners() {
+        $("#filter-btn-title").click(() => {
+            filterAlphabetically("title");    
+        });
+        $("#filter-btn-author").click(() => {
+            filterAlphabetically("author");    
+        });
+        $("#filter-btn-isbn").click(() => {
+            filterNumerically("isbn");    
+        });
+        
+    }
+    
+    function filterAlphabetically(arg) {
 
         let nodes = document.querySelectorAll(".book-row");
-        let arr = Array.from(nodes);
+        let arg_code = -1;
 
-        if ($("#filter-btn-author").children().hasClass("btn-dark")) {
+        arg === "author" ? arg_code = 1 : arg_code = 0;
 
-            $("#filter-btn-author").children().removeClass("btn-dark");
-            $("#filter-btn-author").children().addClass("btn-light");
+        filterReverse(arg, nodes, arg_code);
+    }
 
-            let an = Array.from(document.querySelectorAll(".book-row"));
-            document.querySelector(".book-container").innerHTML = "";
-            
-
-            let na = an.sort(function (a, b) {
-                var im = a.children[0].children[1].innerHTML.toString().localeCompare(b.children[0].children[1].innerHTML.toString());
-                return im;
-            });
-            
-            let container = document.querySelector(".book-container");
-            
-            for (let i = 0; i < na.length; i++) {
-                container.appendChild(na[i]);
-            }
-
-            
-
-        } else {
-            $("#filter-btn-author").children().addClass("btn-dark");
-            $("#filter-btn-author").children().removeClass("btn-light");
-
-            let an = Array.from(document.querySelectorAll(".book-row"));
-            document.querySelector(".book-container").innerHTML = "";
-            
-
-            let na = an.sort(function (a, b) {
-                var im = a.children[0].children[1].innerHTML.toString().localeCompare(b.children[0].children[1].innerHTML.toString());
-                return im*(-1);
-            });
-            
-            let container = document.querySelector(".book-container");
-            
-            for (let i = 0; i < na.length; i++) {
-                container.appendChild(na[i]);
-            }
-        }
-    });
+    function filterNumerically(arg) {
+        let nodes = document.querySelectorAll(".book-row");
+        let arg_code = 2;
+    
+    
+        filterReverse(arg, nodes, arg_code);
+    }
 });
+    /**************************************************************************/
+
+
+function filterReverse(arg, nodes, arg_code) {
+    
+    if ($("#filter-btn-" + arg).children().hasClass("btn-dark")) {
+        
+        $("#filter-btn-"+arg).children().removeClass("btn-dark");
+        $("#filter-btn-" + arg).children().addClass("btn-light");
+        
+        if (arg_code !== 2) {
+            sortDOMAlpha(nodes, arg_code, "asc");
+        } else {
+            sortDOMNum(nodes, 2,"asc");
+        }
+
+        
+
+    } else {
+        $("#filter-btn-"+arg).children().addClass("btn-dark");
+        $("#filter-btn-"+arg).children().removeClass("btn-light");
+
+        if (arg_code !== 2) {
+            sortDOMAlpha(nodes, arg_code, "desc");
+        } else {
+            sortDOMNum(nodes, 2, "desc");
+        }
+    }
+}
+
+function sortDOMAlpha(nodeList, typeOfField, typeOfSort) {
+    let an = Array.from(nodeList);
+    let reverseUno = 0;
+    typeOfSort === "asc" ? reverseUno = 1 : reverseUno = -1;
+
+    document.querySelector(".book-container").innerHTML = "";
+
+    let na = an.sort(function (a, b) {
+        var im = a.children[0].children[typeOfField].innerHTML.toString().localeCompare(b.children[0].children[typeOfField].innerHTML.toString());
+        return im*reverseUno;
+    });
+    
+    let container = document.querySelector(".book-container");
+    
+    for (let i = 0; i < na.length; i++) {
+        container.appendChild(na[i]);
+    }
+}
+
+function sortDOMNum(nodeList, typeOfField,typeOfSort) {
+    let an = Array.from(nodeList);
+    let container = document.querySelector(".book-container");
+    let reverseUno;
+    typeOfSort === "asc" ? reverseUno = true : reverseUno = false;
+
+    document.querySelector(".book-container").innerHTML = "";
+
+    if (reverseUno) {
+        let na = an.sort(function (a, b) {
+            if (parseInt(a.children[0].children[typeOfField].innerHTML) > parseInt(b.children[0].children[typeOfField].innerHTML)) {
+                return 1;
+            } else if (parseInt(a.children[0].children[typeOfField].innerHTML) < parseInt(b.children[0].children[typeOfField].innerHTML)) {
+                return -1;
+            } else return 0;
+        });
+
+        for (let i = 0; i < na.length; i++) {
+            container.appendChild(na[i]);
+        }  
+
+    } else {
+        let na = an.sort(function (a, b) {
+            if (parseInt(a.children[0].children[typeOfField].innerHTML) < parseInt(b.children[0].children[typeOfField].innerHTML)) {
+                return 1;
+            } else if (parseInt(a.children[0].children[typeOfField].innerHTML) > parseInt(b.children[0].children[typeOfField].innerHTML)) {
+                return -1;
+            } else return 0;
+        });
+
+        for (let i = 0; i < na.length; i++) {
+            container.appendChild(na[i]);
+        }  
+    }
+}
