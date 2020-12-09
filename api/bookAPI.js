c399e8611339134d728b7b1ad7e6b09bd7b5fc3f
hyperinactive <hyperinactive.box@gmail.com>
2020-12-09 18:10:30 +0100

    Search and data loading

--- api/bookAPI.js
+++ api/bookAPI.js
@@ -2,30 +2,48 @@ const Author = require('../models/authorModel').Author;
 const Book = require('../models/bookModel').Book;
 
 exports.getBooks = async (req, res, next) => {
-
   // you're not making this easy, mongoose... just give me the count of stuff
-  const numberOfElements = await Book.find({}).countDocuments(function (err, count) {
+
+  console.log(req.body);
+  const searchParam = new RegExp(req.body.search, 'i');
+  console.log(searchParam);
+
+  let query = {};
+
+  if (searchParam !== '/(?:)/i') {
+    query = {
+      $or: [{ title: searchParam }, { author: searchParam }],
+    };
+  }
+
+  const numberOfElements = await Book.find(query).countDocuments(function (
+    err,
+    count
+  ) {
     if (err) {
       console.log(err);
     }
   });
 
-  const fbooks = await Book.find({}, null, { sort: { _id: req.body.sort } }, (err, foundBooks) => {
+  const fbooks = await Book.find(query, (err, foundBooks) => {
     if (err) {
       return res.json({
         error: err,
       });
     }
-  }).skip(req.body.skip).limit(req.body.limit);
+  })
+    .skip(req.body.loadedBooks)
+    .limit(req.body.rate)
+    .sort(null);
 
   // if the last of the items' shipped, send a flag to stop sending requests
-  return numberOfElements <= req.body.limit + req.body.skip ?
-    res.status(200).json({
-    empty: true,
-    data: fbooks,
-    })
+  return numberOfElements <= req.body.rate + req.body.loadedBooks
+    ? res.status(200).json({
+        empty: true,
+        data: fbooks,
+      })
     : res.status(200).json({
-    empty: false,
-    data: fbooks,
-    });
+        empty: false,
+        data: fbooks,
+      });
 };
--- public/scripts/loadBooks.js
+++ public/scripts/loadBooks.js
@@ -1,8 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
-  let loadedBooks = 0;
-  const rate = 8;
-  let isEmpty = false;
+  // search params object
+    // -------------------------------
+  globals = {
+    loadedBooks: 0,
+    rate: 8,
+    isEmpty: false,
+    sort: -1,
+    search: '',
+  }
 
+  // hard reset
+  const resetSearchParams = () => {
+    globals.loadedBooks = 0;
+    globals.rate = 8;
+    globals.isEmpty = false;
+    globals.sort = -1;
+    globals.search = '';
+  }
+  // -------------------------------
+
+
+  // Intersect Observer
+  // -------------------------------
   options = {
     root: null,
     rootMargin: '0px',
@@ -18,6 +37,27 @@ document.addEventListener('DOMContentLoaded', function () {
 
   const observer = new IntersectionObserver(handleIntersect, options);
   observer.observe(document.querySelector('footer'));
+  // -------------------------------
+
+
+  $('#search').click(() => {
+    console.log('clicked');
+
+    // assume the client want to reset the whole thing
+    resetSearchParams();
+    globals.search = $('#search-input').val();
+
+    $('#search-input').val('');
+
+    // reset the global search object and clear the container
+    console.log('globabs has been reset');
+    console.log(globals);
+    console.log('------------------------------');
+    $('.grid-container').html('');
+
+    // stop uncommenting this, the observer will call it automatically when the wrapper gets emptied
+    // loadBooks()
+  });
 
   const loadBooks = () => {
     // loaded books isn't the actual number of loaded books
@@ -25,7 +65,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const requestLoading = async () => {
       // if the db has no more items to send, don't send any more requests
-      if (isEmpty === true) {
+      if (globals.isEmpty === true) {
         return;
       }
 
@@ -34,21 +74,19 @@ document.addEventListener('DOMContentLoaded', function () {
       const tmp = location.split('/');
       const url = tmp[0] + '//' + tmp[2] + '/bookAPI';
 
-      const data = {
-        limit: rate,
-        skip: loadedBooks,
-        sort: -1
-      };
+      console.log(globals);
 
       fetch(url, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(data),
+        body: JSON.stringify(globals),
       })
         .then((response) => response.json())
         .then((data) => {
+          console.log(data);
+          console.log(`--------------------------`);
           // if the last payload turned out empty, no more items are to be requested
 
           // <button class="btn-danger page-btn" type="submit">Page</button>
@@ -79,13 +117,13 @@ document.addEventListener('DOMContentLoaded', function () {
               </div>
             </form>`;
             $('.grid-container').append(bookItem);
+          globals.loadedBooks++;
           });
           if (data.empty === true) {
-            isEmpty = true;
+            globals.isEmpty = true;
             return;
           }
         });
-      loadedBooks += rate;
     };
 
     requestLoading();
